4efdff9732571b3a9441fcf5f0acaaa6502c3683
meipulseras <meipulseras@gmail.com>
2023-10-25 21:28:33 -0300

    modal fotos y h1 con bienvenida

---
+++ public/js/modal.js
@@ -0,0 +1,21 @@
+var modal = document.getElementById("modalfotos");
+
+const images = document.getElementsByClassName("prod");
+
+const clicked = e => {
+    console.log(e.target.src);
+    modal.style.display = "block";
+    modalImg.src = e.target.src;
+}
+
+for(let img of images){
+    img.addEventListener("click", clicked);
+} 
+
+var modalImg = document.getElementById("img01")
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function(){
+    modal.style.display = "none";
+}
