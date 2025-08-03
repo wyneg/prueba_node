77f8aca4f69b0c8ca83c6bcbe481b5d6baffcd40
wyneg <wynegsrhuntar@gmail.com>
2025-08-03 16:12:36 -0400

    nuevas fotos y otros arreglos

---
+++ public/js/title.js
@@ -0,0 +1,28 @@
+function titles(){
+
+    document.getElementById(1).title = "1. Pulseras para compartir.";
+    document.getElementById(2).title = "2. Amuleto protector.";
+    document.getElementById(3).title = "3. Pulseras para compartir.";
+    document.getElementById(4).title = "4. Pulsera macramé individual.";
+    document.getElementById(5).title = "5. Pulseras para compartir.";
+    document.getElementById(6).title = "6. Pulseras para compartir.";
+    document.getElementById(7).title = "7. Pulsera cuarzo rosa.";
+    document.getElementById(8).title = "8. Pulsera macramé ceramica.";
+    // document.getElementById(9).title = "09. Pulseras macramé.";
+    // document.getElementById(10).title = "10. Pulsera ágatas 3mm.";
+    // document.getElementById(11).title = "11. Pulseras para compartir.";
+    // document.getElementById(12).title = "12. Pulseras macramé morado.";
+    document.getElementById(1).alt = "Pulseras1";
+    document.getElementById(2).alt = "Pulseras2";
+    document.getElementById(3).alt = "Pulseras3";
+    document.getElementById(4).alt = "Pulseras4";
+    document.getElementById(5).alt = "Pulseras5";
+    document.getElementById(6).alt = "Pulseras6";
+    document.getElementById(7).alt = "Pulseras7";
+    document.getElementById(8).alt = "Pulseras8";
+    // document.getElementById(9).alt = "Pulseras9";
+    // document.getElementById(10).alt = "Pulseras10";
+    // document.getElementById(11).alt = "Pulseras11";
+    // document.getElementById(12).alt = "Pulseras12";
+    
+}
